d6b7695f270571b602054f93d2fdf8106c65c822
Kyrz95 <101722225+Kyrz95@users.noreply.github.com>
2023-09-28 14:35:56 +0200

    Add files via upload

--- p0/regular_expressions.py
+++ p0/regular_expressions.py
@@ -8,15 +8,15 @@ RE0 = "[ab]*a"
 Completa a continuacion las expresiones regulares para los
 ejercicios 1-6:
 """
-RE1 = "a*b*"
-
-RE2 = "a|b|(ab)*a?|(ba)*b?"
-RE3 = "(a*|b*)*(((aa(a*|b*)*)+)|((bb(a*|b*)*)+))*"    
-RE4 = ("")
-RE5 = ""
+RE1 = "[a]*[b]*"
+RE2 = "(b(ab)*(a?))|(a(ba)*(b?))"
+RE3 = "([ab]*bb[ab]*aa[ab]*)|([ab]*aa[ab]*bb[ab]*)"
+RE4 = "(1|2)?(1|2|3|4|5|6|7|8|9)?(0|1|2|3|4|5|6|7|8|9)"
+RE5 = "((ab)|b)*(aa)?((ab)|b)*(aa)?((ab)|b)*a?"
 RE6 = ""
 
 """
 Recuerda que puedes usar el fichero test_p0.py para probar tus
 expresiones regulares.
 """
+
