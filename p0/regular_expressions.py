8b3d1dc896a79bddc6fab033b82acd1a64e17c72
Sergio <sergiohg818@gmail.com>
2023-09-29 10:22:59 +0200

    expresion 6

--- p0/regular_expressions.py
+++ p0/regular_expressions.py
@@ -13,7 +13,7 @@ RE2 = "a|b|(ab)*a?|(ba)*b?"
 RE3 = "([ab]*bb[ab]*aa[ab]*)|([ab]*aa[ab]*bb[ab]*)"
 RE4 = "(([0-2][0-5]?[0-5]?)|(1?[1-9]?[0-9]?)|[0-9])"
 RE5 = "((ab)|b)*(aa)?((ab)|b)*(aa)?((ab)|b)*a?"
-RE6 = ""
+RE6 = "(0?(10)*(11)?(01)*(00)?(10)*1?)|(1?(01)*(00)?(10)*(11)?(01)*0?)"
 
 """
 Recuerda que puedes usar el fichero test_p0.py para probar tus
