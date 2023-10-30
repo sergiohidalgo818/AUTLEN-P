d454f2bda4e33593923598de9c336100f98f1185
Sergio <sergiohg818@gmail.com>
2023-10-30 19:34:36 +0100

    a medias el 3

--- p1/automata/automata/dfa.py
+++ p1/automata/automata/dfa.py
@@ -1,11 +1,12 @@
 from automata.automaton import State, Transitions, FiniteAutomaton
 from automata.utils import is_deterministic
 from collections import deque
-
+import copy
+import queue
 class DeterministicFiniteAutomaton(FiniteAutomaton):
         
     @staticmethod
-    def to_deterministic(finiteAutomaton):
+    def to_deterministic(finiteAutomaton:FiniteAutomaton):
         """
         Returns an equivalent deterministic finite automaton.
         """
@@ -14,11 +15,38 @@ class DeterministicFiniteAutomaton(FiniteAutomaton):
         from automata.automaton_evaluator import FiniteAutomatonEvaluator
         evaluator = FiniteAutomatonEvaluator(finiteAutomaton)
         
-        #---------------------------------------------------------------------
-        # TO DO: Implement this method...
+        initial = evaluator.current_states
+        table = dict()
 
-        #---------------------------------------------------------------------
-        raise NotImplementedError("This method must be implemented.")
+
+        q = queue.Queue()
+        q.put(evaluator.current_states)
+        newstates=set()
+        newstates.add(evaluator.current_states)
+
+        while not q.empty():
+            state = q.get()
+            evaluator.current_states = state
+            for sym in finiteAutomaton.symbols:
+                evaluator.process_symbol(sym)
+
+                if state not in table.keys():
+                    table[state] = dict()
+                
+                table[state][sym] = evaluator.current_states
+                
+                if evaluator.current_states not in newstates:
+                    newstates.add(evaluator.current_states)
+                    q.put(evaluator.current_states)
+
+
+        
+        
+        trans = Transitions(table)
+        
+        aut = FiniteAutomaton(initial, newstates, finiteAutomaton.symbols, trans)
+
+        return aut
 
     @staticmethod
     def to_minimized(dfa):
